fae52a40f7c751df6ab16f2a2f7e22984061351d
Ezequiel Micha <EzeMicha@MacBook-Air-de-Ezequiel.local>
2021-09-16 18:03:42 -0300

    Ejercicio resuelto por Ezequiel Micha

--- async/userpromesas.js
+++ async/userpromesas.js
@@ -1,25 +1,73 @@
 //EJERCICIO IMPLEMENTAR LOS METODOS A CONTINUACION CON PROMESAS
 
-function getUsers(){
-    
+import fs from "fs/promises";
+const path = "/Users/EzeMicha/Documents/Terciario ORT/21A/TP2/VSC/Repositorio del profesor/TP2-BEAC2-2021/fileSystem/data/users.json";
+// const path = "./users.json";
+
+const new_user = {
+    "_id": "59b99dbbcfa9a34dcd7885c5eee",
+    "name": "J K Rowling",
+    "email": "jk@rowling.uk",
+    "password": "$2b$12$fIYLjRg5tZcTcqy30.MJLO4JRblLhsIhFwYTgUpFBbvnf9S4ONbtWeee"
+};
+
+const user_update = {
+    "_id": "59b99dbbcfa9a34dcd7885c5",
+    "name": "ERNESTO Baratheon",
+    "email": "jack_gleeson@gameofthron.es",
+    "password": "$2b$12$fIYLjRg5tZcTcqy30.MJLO4JRblLhsIhFwYTgUpFBbvnf9S4ONbtW"
+   };
+
+async function getUsers(){
+    return fs.readFile(path, "utf-8");
 }
 
-function getUser(id){
-    
+async function getUser(id){
+    return JSON.parse(await getUsers()).find(user => user._id === id);
 }
 
-function getUserByEmail(email){
 
+async function getUserByEmail(email){
+    return JSON.parse(await getUsers()).find(user => user.email === email);
 }
 
-function pushUser(user){
-    
+async function pushUser(user){
+    const users = JSON.parse(await getUsers());
+    users.push(user);
+    return fs.writeFile(path, JSON.stringify(users, null, " "));
+    // return await getUser(user._id);
 }
 
-function updateUser(user){
-    
+async function updateUser(user){
+    const users = JSON.parse(await getUsers());
+    const indexUser = users.findIndex(u => u._id === user._id);
+    if (users[indexUser] != null) {
+        users[indexUser].name = user.name;
+        users[indexUser].email = user.email;
+        users[indexUser].password = user.password;
+    }
+    else {
+        console.log("Usuario para modificar no encontrado");
+    }
+    return fs.writeFile(path, JSON.stringify(users, null, " "));
+    // return await getUser(id);
 }
 
-function deleteUser(id){
+async function deleteUser(id){
+    const users = JSON.parse(await getUsers()).filter(user => user._id != id);
+    return fs.writeFile(path, JSON.stringify(users, null, " "));
+    // return await getUser(id);
+}
+
+try {
+    console.log(JSON.parse(await getUsers()));
+    console.log(await getUser("59b99dbbcfa9a34dcd7885c5"));
+    console.log(await getUserByEmail("aidan_gillen@gameofthron.es"));
+    await pushUser(new_user);
+    await deleteUser("59b99dbbcfa9a34dcd7885c5eee");
+    await updateUser(user_update);
+    console.log(JSON.parse(await getUsers()));
     
+} catch (error) {
+    console.log(error);
 }
